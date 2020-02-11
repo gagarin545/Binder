e4ce63658acbe9404ee4715992c97ca1e20373b9
gagarin-yf <ura545@mail.ru>
2020-02-11 11:23:42 +0500

    v1

--- src/main/java/ru/Knot.java
+++ src/main/java/ru/Knot.java
@@ -24,20 +24,20 @@ public class Knot extends KnotComponent {
     public void setName(String name) {        this.name = name;    }
     public void difference() {
         double delta =  consumed - knots.stream().mapToDouble(x->  ((KnotComponent) x).getConsumed()).reduce(Double::sum).getAsDouble();
+        System.out.println("Родитель " + name + " имеет разницу " + delta );
         knots.stream()
                 .filter(x-> ((KnotComponent) x).getConsumed() > 0)
                 .peek(x-> ((KnotComponent) x).setDistributed((((KnotComponent) x).getConsumed()/
                         knots.stream().mapToDouble(a->  ((KnotComponent) a).getConsumed()).filter(a-> a > 0 ).reduce(Double::sum).getAsDouble())
                         * delta))
-                .forEach(x-> System.out.println(((KnotComponent) x).getDistributed()));
-        System.out.println("Родитель " + name + " имеет разницу " + delta );
+                .forEach(x-> System.out.println("Узел " +((KnotComponent) x).getName() + " распределенная нагрузка " + ((KnotComponent) x).getDistributed()));
+
     }
     public void print() {
         Iterator iterator = knots.iterator();
         while(iterator.hasNext()) {
             KnotComponent knot = (KnotComponent) iterator.next();
             knot.difference();
-            System.out.println("Узел " + getName() + " распределенная нагрузка " + getDistributed());
             knot.print();
         }
     }
